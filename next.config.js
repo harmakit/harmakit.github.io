362de7d16ed8e2f65240e96509bab413b1fec125
harmakit <pavel.d3v@gmail.com>
2023-12-27 19:56:01 +0300

    init

---
+++ next.config.js
@@ -0,0 +1,14 @@
+// @ts-check
+
+/**
+ * @type {import('next').NextConfig}
+ **/
+const nextConfig = {
+  output: "export",
+  basePath: "/harmakit.github.io",
+  images: {
+    unoptimized: true,
+  },
+};
+
+module.exports = nextConfig;
